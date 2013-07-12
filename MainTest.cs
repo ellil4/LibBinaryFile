ca8a28454865adc2f3386d669d46640f0e2669a7
ellil4 <psychsoftwareproj@163.com>
2013-07-12 12:58:59 +0800

    first commit
    first commit

---
+++ MainTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibBinaryFile
+{
+    class MainTest
+    {
+        static void Main(string[] args)
+        {
+            Reader rd = new Reader(64);
+            rd.Open("C:\\Users\\el\\Documents\\Visual Studio 2010\\Projects\\PsychHRV\\PsychHRV\\bin\\Debug\\HRV1.dat");
+            
+            rd.ReadUnitBy();
+            Console.WriteLine(rd.GetUInt64InBuf(0));
+            Console.WriteLine(rd.GetDoubleInBuf(8));
+
+            rd.ReadUnitBy();
+            Console.WriteLine(rd.GetUInt64InBuf(0));
+            Console.WriteLine(rd.GetDoubleInBuf(8));
+
+            rd.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Reader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace LibBinaryFile
+{
+    public class Reader
+    {
+        public FileStream mStream;
+        private int mUnitLenAsByte = -1;
+        public byte[] mByteBuf = null;
+        //public ulong mCurAtUnit = 0;
+
+        public Reader(int UnitLenAsByte)
+        {
+            mUnitLenAsByte = UnitLenAsByte;
+            mByteBuf = new byte[UnitLenAsByte];
+        }
+
+        public void Open(String path)
+        {
+            mStream = new FileStream(
+                path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            //mCurAtUnit = 0;
+        }
+
+        public int ReadUnitAt(long UnitIndex)
+        {
+            mStream.Seek(UnitIndex * mUnitLenAsByte, SeekOrigin.Begin);
+            int red = mStream.Read(mByteBuf, 0, mUnitLenAsByte);
+            //mCurAtUnit = (ulong)(offUnits + 1);
+            return red;
+        }
+
+        public ulong GetUInt64InBuf(int begat)
+        {
+            return BitConverter.ToUInt64(mByteBuf, begat);
+        }
+
+        public double GetDoubleInBuf(int begat)
+        {
+            return BitConverter.ToDouble(mByteBuf, begat);
+        }
+
+        public int ReadUnitBy()
+        {
+            int red = mStream.Read(mByteBuf, 0, mUnitLenAsByte);
+            //mCurAtUnit++;
+            return red;
+        }
+
+        public void Close()
+        {
+            mStream.Close();
+            mStream = null;
+        }
+    }
+}
